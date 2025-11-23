86be89c27fc3d81f26910ad1b1e414070a60ccdd
Tu Nombre <tu-email@example.com>
2025-11-23 13:22:27 +0100

    Fix encryption salt type, add Netlify config and workspaces, enable successful build

--- packages/web/src/services/encryption.ts
+++ packages/web/src/services/encryption.ts
@@ -23,7 +23,7 @@ class EncryptionService {
         return window.crypto.subtle.deriveKey(
             {
                 name: 'PBKDF2',
-                salt: salt,
+                salt: salt.buffer as ArrayBuffer,
                 iterations: 100000,
                 hash: 'SHA-256'
             },
